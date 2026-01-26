2f839f7f5dbe68488065684e336a94ee7b71a3b3
Pepe <pvidalsalvador@gmail.com>
2026-01-26 20:19:52 +0100

    Render condicional del tab en función del rol del usuario

--- src/app/(protected)/(tabs)/_layout.tsx
+++ src/app/(protected)/(tabs)/_layout.tsx
@@ -1,51 +1,65 @@
 import { Tabs } from "expo-router";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useTheme } from "react-native-paper";
+import { useUserStore } from "@/stores/user.store";
+import { useState } from "react";
 
 export default function TabsLayout() {
-    const theme = useTheme();
+  const theme = useTheme();
+  const { role } = useUserStore();
+  const isAdmin = role != null && role.name == "ADMIN";
 
-    return (
-        <Tabs screenOptions={{
-            tabBarActiveTintColor: theme.colors.primary,
-            tabBarInactiveTintColor: theme.colors.onSurfaceVariant,
-            headerStyle: { height: 0 }
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.onSurfaceVariant,
+        headerStyle: { height: 0 },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="monitor-dashboard"
+              color={color}
+              size={size}
+            />
+          ),
         }}
-        >
+      />
 
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: "Home",
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="monitor-dashboard" color={color} size={size} />
-                    ),
-                }}
+      <Tabs.Screen
+        name="orders"
+        options={{
+          title: "Pedidos",
+          href: isAdmin ? "/orders" : null,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="package-variant"
+              color={color}
+              size={size}
             />
+          ),
+        }}
+      />
 
-            <Tabs.Screen
-                name="orders"
-                options={{
-                    href: null,
-                    title: "Pedidos",
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="package-variant" color={color} size={size} />
-                    ),
-                }}
+      <Tabs.Screen
+        name="customers"
+        options={{
+          title: "Clientes",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-group"
+              color={color}
+              size={size}
             />
-
-            <Tabs.Screen
-                name="customers"
-                options={{
-                    title: "Clientes",
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="account-group" color={color} size={size} />
-                    ),
-                }}
-            />
-
-        </Tabs>
-    );
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
-
